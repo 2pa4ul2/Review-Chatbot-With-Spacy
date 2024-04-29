562cd9cfb7ed034c6cb3fe8098e382096a609c20
Paul Adrian <pauladrian0224@gmail.com>
2024-04-29 23:08:21 +0800

    ✨Text extraction enabled

---
+++ chatbot/chatbot.py
@@ -0,0 +1,11 @@
+import spacy
+
+
+def chatbot(self):
+    pass
+
+def extract_text(self):
+    pass
+
+def generate_question(self):
+    pass
---
+++ chatbot/extract.py
@@ -0,0 +1,18 @@
+#neuralnine
+# #install pdfminer.six
+import re
+
+from pdfminer.high_level import extract_pages, extract_text
+
+
+for page_layout in extract_pages("modelling.pdf"):
+    for element in page_layout:
+        print(element)
+
+text = extract_text("modelling.pdf")
+print(text)
+
+
+#langchain - https://www.youtube.com/watch?v=au2WVVGUvc8
+
+#spacy - https://www.youtube.com/watch?v=dIUTsFT2MeQ
